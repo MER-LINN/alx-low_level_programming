b5c97f2108680f39813f58fbe834392fc77d9982
MER-LINN <fcollinz4u@gmail.com>
2022-09-08 18:05:41 -0700

    Quick fix for Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,11 @@
 
 /**
  * main - Entry point
- * 
- *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");   
-       	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
