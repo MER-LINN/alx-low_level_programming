a659b0f278ad49b03faca3c95ed2192a74f2947a
MER-LINN <fcollinz4u@gmail.com>
2022-09-08 19:11:49 -0700

    Quick fix for Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point*
- * Return: 0
+ * main - Entry point
+ * Return: Always 0
  */
 
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");   
 	return (0);
-}	
+}
