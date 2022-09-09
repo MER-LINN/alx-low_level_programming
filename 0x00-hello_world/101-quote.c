94da1a99f949b1803a511dd3a5df3c709b289338
MER-LINN <fcollinz4u@gmail.com>
2022-09-09 15:03:11 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry Point
+ * A program to print Matters on C
+ * Return: 1 (Success)
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
