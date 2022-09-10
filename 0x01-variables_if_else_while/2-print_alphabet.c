dfa8164c626d20a0659566c37897caf1387b7a88
MER-LINN <fcollinz4u@gmail.com>
2022-09-10 07:54:11 -0700

    Task 2 again

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry Point
  * Description: a program that prints the alphabet in lowercase
- * Return = 1
+ * Return = 0
 */
 
 int main(void)
 {
-	int ch;
+	ch = 'a';
 
 	for (ch = 'a'; ch <= 'z'; ++ch)
 	putchar(ch);
-	return (1);
+	return (0);
 }
