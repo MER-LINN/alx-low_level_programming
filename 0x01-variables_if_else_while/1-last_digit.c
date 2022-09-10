200237c38ed1aed95d2676a4a989378c1237ce63
MER-LINN <fcollinz4u@gmail.com>
2022-09-10 04:33:55 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,26 +3,25 @@
 #include <stdio.h>
 
 /*
- * main - Variables
- * betty style doc for function main goes there
- * Return: 0
+ * main - 'Variables'
+ * Description: This program will assign a random number to the variable n
+ * Return = 0
 */
-
-int main(void)
+int main(void);
 {
 	int n;
 
 	srand(time(0));
 	in = rand() - RAND_MAX / 2;
-	if (n > 5);
+	if (n > 5)
 	{
 		printf("%d and is greater than 5\n", n);
 	}
-	else if (n == 0);
+	else if (n == 0)
 	{
-		printf("%d and is 0");
+		printf("%d and is 0\n", n);
 	}
-	else 
+	else if (n < 6 != 0)
 	{
 		printf("%d and is less than 6 and not 0\n", n);
 	}
