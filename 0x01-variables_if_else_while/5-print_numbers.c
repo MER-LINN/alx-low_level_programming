3ae888c3794d8b3f838c4890a5d5b813bf6042bf
MER-LINN <fcollinz4u@gmail.com>
2022-09-12 18:09:20 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Entry Point
+ * Description: A program that prints single characters in base 10
+ * Return: 0
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch >= 0; ch <= 9; ch++)
+	printf(ch);
+	printf('\n');
+	return (0);
+}
