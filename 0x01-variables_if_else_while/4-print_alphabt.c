bac6b2ca4b201b031d52f061fda8ea14420b7f44
MER-LINN <fcollinz4u@gmail.com>
2022-09-12 17:44:29 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Entry Point
+ * Description: A program that prints alphabets with exceptions
+ * Return: 0
+*/
+int main(void)
+{
+	int(ch);
+	for (ch = "a..z"; ch != 'q'; ch != 'e'; ch++)
+	putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
