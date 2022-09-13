e58f84f305bae076460dbb30010789fa019d82e9
MER-LINN <fcollinz4u@gmail.com>
2022-09-12 19:05:26 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: A program that prints alphabets in reverse
+ * Return: 0
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch <= 'a')
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
