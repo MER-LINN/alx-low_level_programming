eed3d0c8b9ac9c49e06dfe39729cfdecb00972cd
MER-LINN <fcollinz4u@gmail.com>
2022-09-12 18:38:17 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: A program that prints numbers
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
