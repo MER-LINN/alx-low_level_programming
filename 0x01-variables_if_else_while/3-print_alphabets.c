bf6531fcb6d0774eb8b0b90e8101f32a7d89b59b
MER-LINN <fcollinz4u@gmail.com>
2022-09-12 17:26:53 -0700

    Task 3 final commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,13 +5,16 @@
  * Description: A program that prints alphabets in upper and lowercase
  * Return: 0
 */
-
 int main(void)
 {
 	int ch;
 
-	for (ch = "A..Z", ch = "a..z")
+	for (ch = 'a'; ch <= 'z'; ch++)
 	putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	putchar(ch);
+	putchar('\n');
 	return (0);
 }
 
